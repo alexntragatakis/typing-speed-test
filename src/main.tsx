5b0e46e90c734db85e2d096693bf58ddc373db15
alexntragatakis <205552405+alexntragatakis@users.noreply.github.com>
2025-12-24 23:01:38 -0500

    Install react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.tsx";
 
+const router = createBrowserRouter([
+  { path: "/typing-test", element: <App /> },
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
