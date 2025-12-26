7df176fe0fc061fb22ba3d47bf2993d78768261e
alexntragatakis <205552405+alexntragatakis@users.noreply.github.com>
2025-12-26 18:43:42 -0500

    Create testOptions and style interfaces

---
+++ src/utils/testOptions.ts
@@ -0,0 +1,12 @@
+export interface testOptions {
+  wordCount: number;
+  style: style;
+}
+
+export interface style {
+  backGroundClassName: string;
+  textBoxClassName: string;
+  backClassName: string;
+  frontCorrClassName: string;
+  frontIncClassName: string;
+}
