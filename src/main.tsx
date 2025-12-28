d2294e8b3e9f259696d3837fc90ed5abaf6624d6
alexntragatakis <205552405+alexntragatakis@users.noreply.github.com>
2025-12-28 02:54:27 -0500

    refactor: add root component with TestOptionsContext provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles/variables.css";
+
 import App from "./App.tsx";
 import Options from "./components/Options/Options.tsx";
+
+import {
+  TestOptionsContext,
+  useTestOptionsContext,
+} from "./context/TestOptionsContext.ts";
 
 const router = createBrowserRouter([
   { path: "/typing-speed-test/", element: <App /> },
@@ -13,6 +20,26 @@ const router = createBrowserRouter([
     element: <Options onSave={(data) => {}} />,
   },
 ]);
+
+const Root = () => {
+  const [testOptions, setTestOptions] = useState({
+    /* Default Test Settings */
+    wordCount: 25,
+    style: {
+      backGroundColor: "--default-pagebg",
+      textBoxColor: "--default-boxbg",
+      backColor: "--default-black-back-text",
+      frontCorrColor: "--default-correctly-typed-text",
+      frontIncColor: "--default-incorrectly-typed-text",
+    },
+  });
+
+  return (
+    <TestOptionsContext.Provider value={{ testOptions, setTestOptions }}>
+      <RouterProvider router={router} />
+    </TestOptionsContext.Provider>
+  );
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
