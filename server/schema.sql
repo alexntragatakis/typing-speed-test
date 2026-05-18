68e7176358e17d24530041a73c21e60c1f0b5c0e
alexntragatakis <205552405+alexntragatakis@users.noreply.github.com>
2026-05-18 14:27:47 -0400

    feat: create db schema for scores

---
+++ server/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS scores (
+  id         SERIAL PRIMARY KEY,
+  username   VARCHAR(30) NOT NULL DEFAULT 'Anonymous',
+  wpm        INTEGER NOT NULL,
+  accuracy   NUMERIC(5, 2) NOT NULL,
+  word_count INTEGER NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
