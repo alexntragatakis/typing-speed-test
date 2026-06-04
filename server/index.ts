425a0a8f77ae464c63877b377fa425bd2bc3a6dc
alexntragatakis <205552405+alexntragatakis@users.noreply.github.com>
2026-06-04 02:25:42 -0400

    fix: name wildcard for express 5 syntax

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ app.use("/api/scores", scoresRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../dist")));
 
-  app.get("*", (_req, res) => {
+  app.get("/{*path}", (_req, res) => {
     res.sendFile(path.join(__dirname, "../dist/index.html"));
   });
 }
